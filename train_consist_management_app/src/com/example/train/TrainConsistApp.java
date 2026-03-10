6476b5df5c96dc052ed2bd2e255ac9a5eb967ef0
Bathinanna <bathinibathinanna1432@gmail.com>
2026-03-10 12:22:37 +0530

    coded to track unique ids in traon consist management app

--- train_consist_management_app/src/com/example/train/TrainConsistApp.java
+++ train_consist_management_app/src/com/example/train/TrainConsistApp.java
@@ -1,15 +1,18 @@
 package com.example.train;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TrainConsistApp {
     public static void main(String[] args) {
+        
+       
         System.out.println("UC2 - Add Passenger Bogies to Train");
-
+       
         List<String> passengerBogies = new ArrayList<>();
 
-        // Add bogies
         passengerBogies.add("Sleeper");
         passengerBogies.add("AC Chair");
         passengerBogies.add("First Class");
@@ -17,13 +20,11 @@ public class TrainConsistApp {
         System.out.println("\nAfter Adding Bogies:");
         System.out.println("Passenger Bogies : " + passengerBogies);
 
-        // Remove one bogie
         passengerBogies.remove("AC Chair");
 
         System.out.println("\nAfter Removing 'AC Chair':");
         System.out.println("Passenger Bogies : " + passengerBogies);
 
-        // Check existence
         boolean containsSleeper = passengerBogies.contains("Sleeper");
 
         System.out.println("\nChecking if 'Sleeper' exists:");
@@ -33,5 +34,26 @@ public class TrainConsistApp {
         System.out.println(passengerBogies);
 
         System.out.println("\nUC2 operations completed successfully...");
+
+      
+
+        System.out.println("UC3 - Track Unique Bogie IDs");
+      
+
+        Set<String> bogieIds = new HashSet<>();
+
+        // Add IDs (with intentional duplicates)
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        bogieIds.add("BG103");
+        bogieIds.add("BG101"); // duplicate
+        bogieIds.add("BG102"); // duplicate
+        bogieIds.add("BG104");
+
+        System.out.println("\nBogie IDs added (including duplicates): BG101, BG102, BG103, BG101, BG102, BG104");
+        System.out.println("Final Unique Bogie IDs (HashSet): " + bogieIds);
+        System.out.println("Total unique bogie IDs count: " + bogieIds.size());
+
+        System.out.println("\nUC3 operations completed successfully...");
     }
 }
